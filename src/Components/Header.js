2465425c815b303d08cdb28edb060e750d83f08a
Chaolun Feng <clfeng@uw.edu>
2021-03-24 21:44:43 -0700

    Update formatting and UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,52 +1,41 @@
-import { motion } from 'framer-motion';
 import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
 class Header extends Component {
   render() {
-
-      let greeting= "Welcome to my website!";
-      let description= "I am a Junior @UW in Seattle. I am passionate about Full Stack Development and is actively looking for internship oppotunities.";
-
-
+    let greeting = "Welcome to my website!";
+    let description = "I am a Junior @UW in Seattle. I am passionate about Full Stack Development and is actively looking for internship oppotunities.";
     return (
       <header id="home">
-
-      <nav id="nav-wrap">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}  className="current"><a className="smoothscroll" href="#home">Home</a></motion.li>
-	         <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a className="smoothscroll" href="#resume">About</a></motion.li>
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+          <ul id="nav" className="nav">
+            <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="current"><a className="smoothscroll" href="#home">Home</a></motion.li>
+            <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a className="smoothscroll" href="#resume">About</a></motion.li>
             <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a className="smoothscroll" href="#portfolio">Portfolio</a></motion.li>
             <motion.li whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} ><a className="smoothscroll" href="#about">Contact</a></motion.li>
-         </ul>
-
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
+          </ul>
+        </nav>
+        <div className="row banner">
+          <div className="banner-text">
             <motion.div whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.9 }} >
-               <h1 className="responsive-headline">I'm Chaolun Feng.</h1>
+              <h1 className="responsive-headline">I'm Chaolun Feng.</h1>
             </motion.div>
             <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-               <h2>{greeting}</h2>
+              <h2>{greeting}</h2>
             </motion.div>
             <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} >
-               <h3>{description}</h3>
+              <h3>{description}</h3>
             </motion.div>
             <hr />
-         </div>
-      </div>
-
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+          </div>
+        </div>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#resume"><i className="icon-down-circle"></i></a>
+        </p>
+      </header>
     );
   }
 }
-
 export default Header;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
 class Portfolio extends Component {
   render() {
-
-
-
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out My Work.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
               <div key="Personal website" className="columns portfolio-item">
                 <div className="item-wrap">
                   <a href="https://chaolunfeng.github.io" title="Personal website">
                     <img alt="Personal Website" src="images/website.png" />
-                      <div className="overlay">
-                          <div className="portfolio-item-meta">
-                            <h5>Personal Website</h5>
-                            <p>Built using JavaScript and React</p>
-                          </div>
-                        </div>
-                      <div className="link-icon"><i className="fa fa-link"></i></div>
+                    <div className="overlay">
+                      <div className="portfolio-item-meta">
+                        <h5>Personal Website</h5>
+                        <p>Built using JavaScript and React</p>
+                      </div>
+                    </div>
+                    <div className="link-icon"><i className="fa fa-link"></i></div>
                   </a>
                 </div>
               </div>
-            </div>
+            </motion.div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
